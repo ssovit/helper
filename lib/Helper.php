ca9a5be8ca540ad8e9c061b6a522152a7c336288
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2020-02-19 11:57:13 +0545

    update

--- lib/Helper.php
+++ lib/Helper.php
@@ -23,11 +23,14 @@ if (!class_exists('\Sovit\Helper')) {
          * @param $value
          * @param $hideEmpty
          */
-        public static function get_terms($taxonomy = 'category', $key = "slug", $value => "name", $hideEmpty = true) {
+        public static function get_terms($taxonomy = 'category', $key = "slug", $value = "name", $hideEmpty = true) {
             $terms = get_terms([
                 'taxonomy'   => $taxonomy,
                 'hide_empty' => $hideEmpty,
             ]);
+            if (is_wp_error($terms)) {
+                return [];
+            }
 
             return wp_list_pluck($terms, $value, $key);
         }
